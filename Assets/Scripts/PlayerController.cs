0d23004de321e5365b3396efd268e6cc2cc26dcd
dars01012 <dars01012@gmail.com>
2026-05-08 22:50:46 -0600

    no message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Gatherinput m_gatherinput;
     private Transform m_transform;
     [SerializeField] private float speed;
+    [SerializeField] private int direction = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +21,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        m_rigidbody2D.linearVelocity = new Vector2( speed * m_gatherinput.ValueX, m_rigidbody2D.linearVelocityY);
+        Move();
+    }
+    private void Move()
+    {
+        Flip();
+       m_rigidbody2D.linearVelocity = new Vector2( speed * m_gatherinput.ValueX, m_rigidbody2D.linearVelocityY); 
+    }
+
+    private void Flip()
+    {
+        if (m_gatherinput.ValueX * direction < 0)
+        {
+            m_transform.localScale = new Vector3(-m_transform.localScale.x,1,1);
+            direction *= -1; 
+        }
     }
 }
